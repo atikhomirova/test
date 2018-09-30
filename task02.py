9f9b1cb5e1cd8650edb89d16880652a4f8213170
atikhomirova <an.tikhomirovaa@gmail.com>
2018-09-30 13:52:34 +0300

    modified task02 on test2

--- task02.py
+++ task02.py
@@ -3,7 +3,7 @@
 
 if __name__ = __main__:
 
-    def fib(n):
+    def FIB(n):
        if n == 0:
            return 0
        elif n == 1:
@@ -11,8 +11,8 @@ if __name__ = __main__:
        else:
            return fib(n - 1) + fib(n - 2)
     
-    n = 5
+    n = 21
     
     for i in range(n):
-        print(fib(i))
+        print(i)
 
