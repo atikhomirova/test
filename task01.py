0022408a706e4b4a8e7419fb1d3bac5836b3aa3e
atikhomirova <an.tikhomirovaa@gmail.com>
2018-09-30 13:23:28 +0300

    modified task01

--- task01.py
+++ task01.py
@@ -1,8 +1,6 @@
 # Task 01
 # Find most frequent element in the list
+if __name__ == "__main__":
 
-lst = [4, 2, 3, 4, 4, 4, 5, 5, 5, 5]
-
-print(max(set(lst),key=lst.count))
-print(min(set(lst),key=lst.count))
-
+    lst = [4, 2, 3, 4, 4, 4, 5, 5, 5, 5]
+    print(max(set(lst),key=lst.count))
